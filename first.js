77e5c32f85aa2a907501f0551d6cc15eeda18609
Priyanshu Patwal <priyanshupatwal22@gmail.com>
2025-02-05 08:35:48 +0530

    payment updated after footer

--- first.js
+++ first.js
@@ -5,5 +5,10 @@ console.log(button);
 const login = "login page edit";
 console.log(login);
 
+<<<<<<< HEAD
 const footer = "Footer added in our website";
 console.log(footer);
+=======
+const payment = "integreted the payment";
+console.log(payment);
+>>>>>>> 3994f2d (payment added)
