71fc722fa686f5e0a3bc93e2891b8e673ed83cf6
Priyanshu Patwal <priyanshupatwal22@gmail.com>
2025-02-05 18:43:23 +0530

    Latest

--- first.js
+++ first.js
@@ -5,16 +5,11 @@ console.log(button);
 const login = "login page edit";
 console.log(login);
 
-<<<<<<< HEAD
 const footer = "Footer added in our website";
 console.log(footer);
-=======
+
 const payment = "integreted the payment";
 console.log(payment);
-<<<<<<< HEAD
->>>>>>> 3994f2d (payment added)
-=======
 
 const upi = "Integrated the UPI";
 console.log(upi);
->>>>>>> 65ec229 (upi added)
